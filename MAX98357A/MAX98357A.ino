95ea874b87143b83bd8869ced17265d774172c08
Ali Hassan <alihassan.pk019@gmail.com>
2025-03-26 10:45:06 +0500

    Final_Code

--- MAX98357A/MAX98357A.ino
+++ MAX98357A/MAX98357A.ino
@@ -1,9 +1,18 @@
-void setup() {
-  // put your setup code here, to run once:
+#include "AudioTools.h"
+#include "BluetoothA2DPSink.h"
 
+I2SStream i2s;
+BluetoothA2DPSink a2dp_sink(i2s);
+
+void setup() {
+    auto cfg = i2s.defaultConfig();
+    cfg.pin_bck = 27;
+    cfg.pin_ws = 26;
+    cfg.pin_data = 25;
+    i2s.begin(cfg);
+
+    a2dp_sink.start("MyMusic");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
 }
